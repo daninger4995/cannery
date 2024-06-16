0db5accc43141f63b336f1ed32db2f44adbb3c60
shibao <shibao@shibao.dev>
2024-06-16 12:13:34 -0400

    downgrade versions until hex is supported in build

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cannery.MixProject do
     [
       app: :cannery,
       version: "0.9.10",
-      elixir: "1.17.0",
+      elixir: "1.16.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),

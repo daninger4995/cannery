366a6d160d5f7fd13f81b5b77a4d929e1da2e76a
shibao <shibao@bubbletea.dev>
2025-04-05 01:20:35 +0000

    format migration

--- priv/repo/migrations/20250405004036_set_utc_datetime.exs
+++ priv/repo/migrations/20250405004036_set_utc_datetime.exs
@@ -47,6 +47,5 @@ defmodule Cannery.Repo.Migrations.SetUtcDatetime do
       modify :inserted_at, :utc_datetime_usec, from: :naive_datetime
       modify :updated_at, :utc_datetime_usec, from: :naive_datetime
     end
-
   end
 end

e853f9e24f6bd6438077067f646f9c363c750ff4
shibao <shibao@shibao.dev>
2025-04-05 04:20:07 +0000

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cannery.MixProject do
   def project do
     [
       app: :cannery,
-      version: "0.9.13",
+      version: "0.9.14",
       elixir: "1.18.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

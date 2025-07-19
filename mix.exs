14615ac107f51dd1ab80bbe6dcf0acbfe1b38f39
shibao <shibao@bubbletea.dev>
2025-07-19 00:45:53 +0000

    fix bug with ammo type selector

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cannery.MixProject do
   def project do
     [
       app: :cannery,
-      version: "0.9.15",
+      version: "0.9.16",
       elixir: "1.18.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

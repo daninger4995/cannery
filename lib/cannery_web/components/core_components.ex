b916f623790329536b6dceb34055e6e1054a14aa
shibao <shibao@shibao.dev>
2026-03-18 05:05:11 +0000

    run mix format

--- lib/cannery_web/components/core_components.ex
+++ lib/cannery_web/components/core_components.ex
@@ -417,7 +417,15 @@ defmodule CanneryWeb.CoreComponents do
   def input(%{type: "select"} = assigns) do
     ~H"""
     <div class={@container_class}>
-      <.label for={@id} class={[@hidden_label && "sr-only", "block text-sm leading-6 text-zinc-900 dark:text-neutral-200"]}>{@label}</.label>
+      <.label
+        for={@id}
+        class={[
+          @hidden_label && "sr-only",
+          "block text-sm leading-6 text-zinc-900 dark:text-neutral-200"
+        ]}
+      >
+        {@label}
+      </.label>
       <select
         id={@id}
         name={@name}
